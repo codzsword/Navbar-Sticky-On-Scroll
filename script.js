9e3ac99e74948ed7c8de4f36c5090b4c9fe6ff3a
CodzSword <124258380+codzsword@users.noreply.github.com>
2024-05-22 12:05:27 +0545

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function stickyNav() {
+  var headerHeight = document.querySelector("#about").offsetHeight / 2;
+  var navbar = document.querySelector("nav");
+  var scrollValue = window.scrollY;
+
+  if (scrollValue > headerHeight) {
+    navbar.classList.add("header-sticky");
+  } else {
+    navbar.classList.remove("header-sticky");
+  }
+}
+
+window.addEventListener("scroll", stickyNav);
